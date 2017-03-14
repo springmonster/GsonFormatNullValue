5adfb755ce1596acdaf29e397244059f289cd890
kuanghaochuan <kuanghaochuan@wisetv.com.cn>
2017-03-14 16:23:59 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GsonFormatNullValue
 Modify gson format, if value is null return default value in getter method
+
+String: return ""
+List: return new ArrayList<>();
+.etc
---
+++ src/org/gsonformat/intellij/common/TextUtils.java
@@ -0,0 +1,10 @@
+package org.gsonformat.intellij.common;
+
+/**
+ * Created by kuanghaochuan on 2017/3/14.
+ */
+public class TextUtils {
+    public static boolean isEmpty(String input) {
+        return input == null || input.length() == 0;
+    }
+}
